aa4b4562a9d01c5d8d0888d6aad8c6526b71f2f3
Akinkuowo <akinseun1990@gmail.com>
2020-02-11 09:53:08 +0100

    adding header component to project

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import { Route } from 'react-router-dom';
 import HomePage from './Page/HomePage/homePage';
 import Hats from './Page/HomePage/Hats/hats';
 import ShopPage from './Page/ShopPage/shopPage';
+import Header from './components/Header/header';
 
 
 const App = () => {
 	
 		return (
 			<div >
+				<Header />
 				<Route exact path="/" component={HomePage} />
 				<Route  path="/shop/hats" component={Hats} />
 				<Route path="/shop" component={ShopPage} />
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+// import {ReactComponent as Logo } from '../../images/crown.svg'
+import logo from '../../images/Logo.png'
+
+import './header.css'
 
 const Header = () => {
-    return <h1 className="">Header</h1>
+    return (
+    <div className="header-container">
+       <Link className="logo-container" to="/">
+             <img className="logo" src={logo} alt=""/>
+       </Link>
+
+       <div className="option-container">
+           <Link className="option" to="/shop">
+                SHOP
+           </Link>
+           <Link className="option" to="/shop">
+                CONTACT 
+           </Link>
+       </div>
+    </div>
+    )
 }
 
 
